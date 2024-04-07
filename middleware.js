0fbf00e1d56fb2c8cdf39b9f9fd7acf165db77d5
Tejas Pawar <tejaspawar70238@gmail.com>
2024-04-07 17:40:25 +0530

    fixing mongoose error

--- middleware.js
+++
@@ -1,9 +0,0 @@
-const User= require('./models/userschema.js');
-
-module.exports.isLoggedIn= (req, res, next)=>{
-    if(!req.isAuthenticated())
-      {
-        return res.redirect('/login');
-      }
-    next();
-}
